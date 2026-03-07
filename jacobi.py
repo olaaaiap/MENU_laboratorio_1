5218960b1698f18523044539c2d408a5cd21b031
Olaia Picabea <72024925+olaaaiap@users.noreply.github.com>
2026-03-07 10:53:19 +0100

    UPDATE jacobi. optimizar for

--- jacobi.py
+++ jacobi.py
@@ -6,11 +6,15 @@ def jacobi(A, b, x0,  tol = 1.e-6, maxit = 100, verbose = True):
     for k in range(1, maxit+1):
         xprev = xk.copy()
         for i in range(len(A)):
-            xk[i] = b[i]
-            for j in range(len(A[i])):
-                if j != i:
-                    xk[i] -= A[i][j]*xprev[j]
-            xk[i] = xk[i] / A[i][i]
+            # xk[i] = b[i]
+            # for j in range(len(A[i])):
+            #     if j != i:
+            #         xk[i] -= A[i][j]*xprev[j]
+            # xk[i] = xk[i] / A[i][i]
+
+            #modo optimizado. podemos usar taichi dot para paralelizar el bucle de fuera.
+            suma = np.dot(A[i], xprev)
+            xk[i] = xprev[i] + 1 / A[i,i]*(b[i]-suma)
 
         error = np.linalg.norm(xk-xprev, np.inf)
         if verbose:
