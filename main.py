72b20690d4345e3c621b05dc45c4583895d7f7cc
Olaia Picabea <72024925+olaaaiap@users.noreply.github.com>
2026-03-07 10:45:07 +0100

    UPDATE main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import sys
 
 def construir_A(n):
     array_4 = np.full(n, 4)
@@ -27,7 +28,13 @@ if __name__ == "__main__":
     b = construir_B(n)
     end_time = time.perf_counter()
 
+    print(f"Tamaño A (bytes): {matrix.nbytes}")
+    print(f"Tamaño b (bytes): {b.nbytes}")
+    print(f"Tamaño A (MB): {matrix.nbytes / (1024 * 1024)}")
+    print(f"Tamaño b (MB): {b.nbytes / (1024 * 1024)}")
+    print(f"Tamaño A (GB): {matrix.nbytes / (1024 * 1024 * 1024)}")
+    print(f"Tamaño b (GB): {b.nbytes / (1024 * 1024 * 1024)}")
+
     duration = end_time - start_time
     print(duration)
     print(matrix)
-    
